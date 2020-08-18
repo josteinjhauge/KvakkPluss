592ea11ed11790002349339c7b7146ab56a77039
Elin Holde Hegsvold <elinholde@gmail.com>
2020-08-18 16:55:09 +0200

    git test

--- app/src/main/java/com/example/mattespill/MainActivity.java
+++ app/src/main/java/com/example/mattespill/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // TODO: dette er en git-test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
