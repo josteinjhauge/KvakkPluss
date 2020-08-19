11f2c44a5be0e7ff7f0a365d654dc451a56d7139
Jostein <josteinjhauge@me.com>
2020-08-19 14:37:18 +0200

    testet og la til navigasjon mellom startskjerm og spillskjerm bare for å teste

--- app/src/main/java/com/example/mattespill/MainActivity.java
+++ app/src/main/java/com/example/mattespill/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.mattespill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // knapp for å skifte activity
+        Button btnGame = findViewById(R.id.btnStartGame);
+        btnGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGameScreen();
+            }
+        });
+    }
+
+    public void openGameScreen(){
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+        Log.i("knapp", "StartGame pressed");
+
     }
 }
