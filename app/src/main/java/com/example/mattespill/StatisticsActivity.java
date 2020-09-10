cbc2998b0dda46b1e8b0705f50ee02654471a09f
Jostein <josteinjhauge@me.com>
2020-09-10 15:55:40 +0200

    listview

--- app/src/main/java/com/example/mattespill/StatisticsActivity.java
+++ app/src/main/java/com/example/mattespill/StatisticsActivity.java
@@ -8,15 +8,29 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import static com.example.mattespill.GameActivity.RESULT;
 import static com.example.mattespill.GameActivity.SHARED_GAME_PREFS;
-import static com.example.mattespill.PrefrencesActivity.GAME_MODE;
-import static com.example.mattespill.PrefrencesActivity.SHARED_PREFS;
 
 public class StatisticsActivity extends AppCompatActivity {
     String result;
+    ListView listView;
+    String [] results = new String[] {
+            "1/5", "5/5", "9/10"
+    };
+    
+    ArrayList<String> resultsList = new ArrayList<>();
+
+    // shared preferences
+    public static final String SHARED_STATS_PREFS = "sharedStatsPrefs";
+    public static final String RESULTS = "results";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +44,16 @@ public class StatisticsActivity extends AppCompatActivity {
         }
 
         loadData();
-        makeList();
         setContentView(R.layout.activity_statistics);
+
+        // TODO: burde lage metode for å fylle arraylist og bare fylle adapter i oncreate
+        makeList(); // denne må fylle arraylist med resultater
+        System.out.println("Arraylist inneholder: " + results);
+
+        listView = findViewById(R.id.listView);
+        ArrayAdapter<String> resultAdapter = new ArrayAdapter<String>(
+                this,R.layout.list_activity, results);
+        listView.setAdapter(resultAdapter);
 
         Button btnBack = findViewById(R.id.btnBackStats);
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +65,7 @@ public class StatisticsActivity extends AppCompatActivity {
     }
     public void back(){
         // TODO: enten legge inn egen lagre knapp, eller bare gjøre det her
-        // saveData();
+        saveData();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
@@ -51,11 +73,22 @@ public class StatisticsActivity extends AppCompatActivity {
     }
 
     public void makeList(){
-
+        Collections.addAll(resultsList, results);
     }
+
     public void loadData(){
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_GAME_PREFS, MODE_PRIVATE);
         result = sharedPreferences.getString(RESULT,"resultat");
+        // TODO: prøve å fikse denne, tror grunnen er at den ikke lagrer hva som er i arraylist
+        // results = sharedPreferences.getString(RESULTS,);
         System.out.println("----:" + result + ":----");
+    }
+
+    public void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_STATS_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(RESULTS, String.valueOf(results));
+        editor.apply();
+        Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show();
     }
 }
